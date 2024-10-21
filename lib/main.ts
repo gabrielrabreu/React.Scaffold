41074827dd2fc602ce4d447cce44ca187e5fbb2e
gabrielrabreu <73451858+gabrielrabreu@users.noreply.github.com>
2024-10-20 21:31:00 -0300

    Add submenu

--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,5 @@
 export * from "./components/ScfButton";
 export * from "./components/ScfHeader";
 export * from "./components/ScfIconButton";
+export * from "./components/ScfPopper";
 export * from "./components/ScfSidebar";
