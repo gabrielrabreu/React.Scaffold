128a8ef252372acfdf17c56e302680ec851f851c
gabrielrabreu <73451858+gabrielrabreu@users.noreply.github.com>
2024-11-02 21:22:09 -0300

    Clean project

--- src/routes/_private/index.lazy.tsx
+++
@@ -1,7 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-import Home from "../../pages/Home";
-
-export const Route = createLazyFileRoute("/_private/")({
-  component: Home,
-});
--- src/routes/auth/login.lazy.tsx
+++
@@ -1,7 +0,0 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
-
-import Login from "../../pages/Login";
-
-export const Route = createLazyFileRoute("/auth/login")({
-  component: Login,
-});
