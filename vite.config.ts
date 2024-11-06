722c4284b512fc93e7b70a2e3816681c2c5c41dc
gabrielrabreu <73451858+gabrielrabreu@users.noreply.github.com>
2024-11-05 22:51:16 -0300

    Add inject css config

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,12 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({
       tsconfigPath: "./tsconfig.app.json",
     }),
